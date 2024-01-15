f3c29dffc2dec87f497a9f37ff7ed7adc3f8f3aa
unknown <danielsafari143@gmail.com>
2024-01-15 15:24:26 +0100

    ADD PIZZA MODESL

--- obj/Debug/net8.0/BlazingPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b866cee6fe0acdb62e1d7108e2bb8dafce10fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
